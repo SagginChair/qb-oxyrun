a1682a279bde51b54b10bf9dc874eef55ec8c95b
SagginChair <Vlezxsaggin1@gmail.com>
2021-10-13 12:15:19 -0500

    price change

--- config.lua
+++ config.lua
@@ -1,14 +1,14 @@
 Config = {}
 
 -- Oxy runs.
-Config.StartOxyPayment = 6500 -- How much you pay at the start to start the run
+Config.StartOxyPayment = 1000 -- How much you pay at the start to start the run
 
 Config.RunAmount = math.random(25,50) -- How many drop offs the player does before it automatixally stops.
 
-Config.Payment = math.random(330, 410) -- How much you get paid when RN Jesus doesnt give you oxy, divided by 2 for when it does.
+Config.Payment = math.random(200, 500) -- How much you get paid when RN Jesus doesnt give you oxy, divided by 2 for when it does.
 
 Config.Item = "oxy" -- The item you receive from the oxy run. Should be oxy right??
-Config.OxyChance = 550 -- Percentage chance of getting oxy on the run. Multiplied by 100. 10% = 100, 20% = 200, 50% = 500, etc. Default 55%.
+Config.OxyChance = 500 -- Percentage chance of getting oxy on the run. Multiplied by 100. 10% = 100, 20% = 200, 50% = 500, etc. Default 55%.
 Config.OxyAmount = 4 -- How much oxy you get when RN Jesus gives you oxy. Default: 4.
 
 Config.BigRewarditemChance = 500 -- Percentage of getting rare item on oxy run. Multiplied by 100. 0.1% = 1, 1% = 10, 20% = 200, 50% = 500, etc. Default 50%.
