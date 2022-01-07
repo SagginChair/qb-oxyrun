61451443ef11cc1956d4386b877a2ec65458491f
SagginChair <Vlezxsaggin1@gmail.com>
2022-01-07 09:42:30 -0600

    btc

--- config.lua
+++ config.lua
@@ -9,9 +9,9 @@ Config.Payment = math.random(400, 700) -- How much you get paid when RN Jesus do
 
 Config.Item = "oxy" -- The item you receive from the oxy run. Should be oxy right??
 Config.OxyChance = 500 -- Percentage chance of getting oxy on the run. Multiplied by 100. 10% = 100, 20% = 200, 50% = 500, etc. Default 55%.
-Config.OxyAmount = 3 -- How much oxy you get when RN Jesus gives you oxy. Default: 4.
+Config.OxyAmount = 1 -- How much oxy you get when RN Jesus gives you oxy. Default: 4.
 
-Config.BigRewarditemChance = 500 -- Percentage of getting rare item on oxy run. Multiplied by 100. 0.1% = 1, 1% = 10, 20% = 200, 50% = 500, etc. Default 50%.
+Config.BigRewarditemChance = 1000 -- Percentage of getting rare item on oxy run. Multiplied by 100. 0.1% = 1, 1% = 10, 20% = 200, 50% = 500, etc. Default 50%.
 Config.BigRewarditem = "markedbills" -- Put a rare item here which will have 50% chance of being given on the run.
 
 Config.OxyCars = "CHECK THE CODE" -- Cars
